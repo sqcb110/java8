c3b681b686d788484a5a3ffcdbc403178fd26cb8
yanchangbo <312210751@qq.com>
2023-07-24 20:09:03 +0800

    2023-07-24-20

---
+++ src/chapter04_arrary/Test_10_Array_Order_Search.java
@@ -0,0 +1,16 @@
+package chapter04_arrary;
+
+/*
+* 数组的顺序查找
+* 对数组元素的顺序没有要求
+* */
+
+public class Test_10_Array_Order_Search {
+    public static void main(String[] args) {
+        // todo 01 查找value第一次在数组中出现的index
+        int[] arr = {4,5,6,7,8,9,1,10};
+        int value = 1;
+        int index = -1;
+        
+    }
+}
