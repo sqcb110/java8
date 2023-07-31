0d6a9707d411fee73c0af8c9fa68a2b07c4ac7bb
cbsq110 <312210751@qq.com>
2023-08-01 01:09:38 +0800

    2023-07-31

--- src/chapter04_arrary/Test_10_Array_Order_Search.java
+++ src/chapter04_arrary/Test_10_Array_Order_Search.java
@@ -11,6 +11,15 @@ public class Test_10_Array_Order_Search {
         int[] arr = {4,5,6,7,8,9,1,10};
         int value = 1;
         int index = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (value == arr[i]){
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println("value第一次在数组中出现的index是：" + index);
         
     }
 }
